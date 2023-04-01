1e17e266ce10bec993cf4f502f1456d9d84f20b4
arindam <arindampradhan562@gmail.com>
2023-04-01 14:25:08 +0530

    qr code generator

---
+++ script.js
@@ -0,0 +1,15 @@
+const wrapper = document.querySelector(".wrapper"),
+    generateBtn = wrapper.querySelector(".form button"),
+    qrInput = wrapper.querySelector(".form input"),
+    qrImg = wrapper.querySelector(".qr-code img")
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value
+    if (!qrValue) return;
+    console.log(qrValue)
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=${qrValue}`
+    qrImg.addEventListener("load", () => {
+        wrapper.classList.add("active")
+    })
+    // wrapper.classList.add("active")
+})
